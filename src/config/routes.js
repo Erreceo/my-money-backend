7f62fc9f7661bc394894f7ebdc93a750fb7c9f78
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2019-09-09 17:29:21 -0300

    Fim do modulo de autenticação do backend

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server){
-  //Definir URL base para todas as rotas
+  /**
+   * Rotas protegidas
+   */
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+  protectedApi.use(auth)
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycle.register(protectedApi, '/billingCycles')
 
-  const router = express.Router()
-  server.use('/api', router)
-
-  // Rotas de Ciclo de Pagamento
-
-  const BillingCycle = require('../api/billingCycle/billingCycleService');
-  BillingCycle.register(router, '/billingCycles')
-
+  /**
+   * Rotas abertas
+   */
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/singup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 
 }
